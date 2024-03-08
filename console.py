6ea45c11deab8faedb8d2b9aabb49063bd615912
Bravin Onwonga <82813496+bravin-onwonga@users.noreply.github.com>
2024-03-09 01:22:52 +0300

    doc(console.py):Entry point of the command interpreter (#9)

---
+++ console.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Script containing the entry point of the command interpreter
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Class inheriting from the cmd.Cmd class
+
+    It contains this methods:
+        quit - exits the program
+        help - lists all methods contained
+
+    Our cutom prompt is (hbnb)
+    """
+
+    prompt = "(hbnb)"
+
+    def do_EOF(self, line):
+        """Ctrl-D to exit the program"""
+        print()
+        return True
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def emptyline(self):
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
