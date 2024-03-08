cb038995c3faa37b97d3e9993f91150081c595c9
Gekko Wrld <gekkowrld@gmail.com>
2024-03-08 14:25:30 +0300

    fix: Update the logic of the script
    update `black` to use a "hard" limit of 78 charactes, should be
    70 characters instead.
    
    Let the tab to space conversion happen on all files.
    
    Signed-off-by: Gekko Wrld <gekkowrld@gmail.com>

--- style.sh
+++ style.sh
@@ -5,26 +5,29 @@ STYLE_FILES=$(find . -name "*.py")
 
 # First change tabs (if available) to spaces
 echo "Converting tabs to spaces..."
-find . -name '*.py' ! -type d -exec bash -c 'expand -t 4 "$0" > /tmp/e && mv /tmp/e "$0"' {} \;
+find . -wholename '*.py' ! -type d -exec bash -c 'expand -t 4 "$0" > /tmp/e && mv /tmp/e "$0"' {} \;
 
-# Check if the tools are available
-if command -v black &> /dev/null; then
-    echo "Running black..."
-    black $STYLE_FILES
-else
-    echo "Consider installing black"
-fi
+for STYLE_FILE in $STYLE_FILES; do
+    # Check if the tools are available
+    if command -v black &> /dev/null; then
+        echo "Running black..."
+        black -l 78 "$STYLE_FILE"
+    else
+        echo -e "Consider installing black\npip install black"
+    fi
 
-if command -v isort &> /dev/null; then
-    echo "Running isort..."
-    isort $STYLE_FILES
-else
-    echo "Consider installing isort"
-fi
+    if command -v isort &> /dev/null; then
+        echo "Running isort..."
+        isort "$STYLE_FILE"
+    else
+        echo -e "Consider installing isort\npip install isort"
+    fi
 
-if command -v pycodestyle &> /dev/null; then
-    echo "Running pycodestyle..."
-    pycodestyle $STYLE_FILES
-else
-    echo "Consider installing pycodestyle"
-fi
+    if command -v pycodestyle &> /dev/null; then
+        echo "Running pycodestyle..."
+        pycodestyle "$STYLE_FILE"
+    else
+        echo -e "Consider installing pycodestyle\npip install pycodestyle"
+    fi
+done
+
