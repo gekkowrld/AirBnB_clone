cfc9a6790f58fe6d277985324b8ed63541d3d1e4
Gekko Wrld <gekkowrld@gmail.com>
2024-03-10 09:33:18 +0300

    chore: Add user class
    Add a simple implementation of the class `user`.
    
    Signed-off-by: Gekko Wrld <gekkowrld@gmail.com>

---
+++ models/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" Create a class user
+
+Manage user functions
+"""
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """User class"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
